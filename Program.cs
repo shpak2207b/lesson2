634786e278f5707250c131d02c66041a4a796244
admin <admin@DESKTOP-DKPHBV2>
2024-09-21 21:36:06 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // =Логические операторы=
+            // == != > < >= <=
+            //bool f;
+
+            //string name = "Alex";
+            //int age = 21;
+            //string str = "15";
+            //age = Convert.ToInt32(str);
+
+            //Console.WriteLine($"имя - {name}, возраст - {age} год");
+
+            //float itog;
+            //int x = 5, y = 2;
+            //itog = Convert.ToSingle(x) / 2;
+            //Console.WriteLine(itog);
+
+
+
+
+            // дз 1 епта
+
+            /////N1
+            //int a = 2;
+            //float b = 14.88f;
+            //double c = 13.37;
+            //string str = "Hello";
+            //bool b2 = true;
+            //long c2 = 324324363646;
+            //Console.WriteLine(b);
+
+
+            ////N2
+            //int amount = 52;
+            //int in_row = 3;
+            //Console.WriteLine(amount / in_row);
+            //Console.WriteLine(amount % in_row);
+
+
+            ///N3
+            //int golda;
+            //Console.WriteLine("введите колво золота");
+            //golda = Convert.ToInt32(Console.ReadLine());
+            //int price = 100;
+            //Console.WriteLine("сколько берем?");
+            //int amount;
+            //amount = Convert.ToInt32(Console.ReadLine());
+            //int am_cr = golda / price;
+            //golda = golda % (price * amount);
+            //Console.WriteLine($"Осталось золлота: {golda}, купили {am_cr} кристаллов");
+
+
+            ///N4 (goida)
+
+            //Console.WriteLine("введите кол-вл человек :");
+            //int len = Convert.ToInt32(Console.ReadLine());
+
+            //int hr, min;
+            //hr = 10 * len / 60;
+            //min = 10 * len % 60;
+
+            //Console.WriteLine($"ждать {hr} часов, {min} минут");
+
+
+            ///N5 (stroki)
+            //Console.WriteLine("введите ваше имя: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("введите ваш знак зодиака: ");
+            //string zodiac = Console.ReadLine();
+            //Console.WriteLine("скок вам лет: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("где вы работаете ");
+            //string job = Console.ReadLine();
+
+
+            //Console.WriteLine($"вас зовут {name}, ваш знак зодика {zodiac}," +
+            //    $" вам {age} год и вы работаете {job}");
+
+            Console.WriteLine("hello wrld");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lesson2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lesson2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4503c898-3ca8-4fbe-9663-004244f6c9fa")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
